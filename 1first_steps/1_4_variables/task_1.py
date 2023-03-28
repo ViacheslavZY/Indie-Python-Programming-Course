dca5acdbd6698321759259df2e365eeb630405a5
Viacheslav <vyzdumaev@gmail.com>
2023-03-28 21:37:11 +0300

    directory location changed

---
+++ 1first_steps/1_4_variables/task_1.py
@@ -0,0 +1,32 @@
+age = int(input())
+a = age + 1
+print(a)
+
+a = int(input())
+q = a * 2
+print(q)
+w = a / 2
+print(w)
+
+a = int(input())
+b = int(input())
+print(a * b, 2 * (a + b))
+
+f = float(input())
+c = (f - 32) * 5 / 9
+print(c)
+
+print((float(input()) - 32) * 5 / 9)
+
+q = float(input())
+print(round(q, 2), round(q, 3))
+
+h1 = int(input())
+m1 = int(input())
+s1 = int(input())
+h2 = int(input())
+m2 = int(input())
+s2 = int(input())
+t1 = h1 * 3600 + m1 * 60 + s1
+t2 = h2 * 3600 + m2 * 60 + s2
+print(t2 - t1)
