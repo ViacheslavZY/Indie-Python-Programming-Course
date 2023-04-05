3165244c30bdc4ee9af727f0d4d9473636e16647
Viacheslav <vyzdumaev@gmail.com>
2023-04-05 21:10:26 +0300

    a new task is created

---
+++ 1first_steps/1_10_logic_type/task_13.py
@@ -0,0 +1,20 @@
+# Необходимо проставить галочки напротив каждого выражения
+# Отметьте верные ячейки
+# Верно решили 5 959 учащихся
+# Из всех попыток 50% верных
+#  Хорошие новости, верно!
+# Ряды:	True	False
+# not True	                 Folse
+#
+# not False                 True
+#
+# not True or False         Folse
+#
+# not True or True          True
+#
+# not (True or False)       Folse
+#
+# not True and not False    Folse
+#
+# not (True and True)       Folse
+#
